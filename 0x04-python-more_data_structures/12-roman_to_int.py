0ad8910242a91415699d2952919186de96103b88
EGHO-LUCKY <egholucky8@gmail.com>
2023-03-10 17:59:16 +0000

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     integer = 0
-    if roman_string:
+    if isinstance(roman_string, str):
         conversions_dict = {
+	    'IV': 4,
+	    'IX': 9,
             'I': 1,
             'V': 5,
+	    'XL': 40,
+	    'XC': 90,
             'X': 10,
             'L': 50,
+	    'CD': 400,
+	    'CM': 900,
             'C': 100,
             'D': 500,
             'M': 1000 }
-        for c in roman_string:
-            integer += conversions_dict.get(c, 0)
+        while roman_string != '':
+            for key in conversions_dict.keys():
+	        if key in roman_string:
+                    integer += conversions_dict.get(key)
+		    roman_string = roman_string.replace(key, '', 1)
+    else:
+        return 0
+
     return integer
