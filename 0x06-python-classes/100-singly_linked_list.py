b6176fcf2d706129c75817d55f4006a049fd8180
EGHO-LUCKY <egholucky8@gmail.com>
2023-10-31 16:50:52 +0000

    Add 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -24,12 +24,40 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if (isinstance(value, Node) == False) and (value is not None):
+        if not isinstance(value, Node) and (value is not None):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Singly linked clasd"""
+    """Singly linked list class"""
     def __init__(self):
+        self.__head = None
 
+    def sorted_insert(self, value):
+        """Inserts a new Node into the correct
+        sorted position in the list (increasing order)
+        """
+        new = Node(value)
+        if self.__head is None:
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """String representation of the Singly linked list class
+        """
+        printlist = []
+        tmp = self.__head
+
+        while tmp is not None:
+            printlist.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ("\n".join(printlist))
